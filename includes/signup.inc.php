a634f2bf90e939e346146cddd90145aa91ad4142
Sablu Hasan <150502592+sablu1234@users.noreply.github.com>
2025-04-17 08:14:19 +0600

    TODAY PRACTICE

---
+++ includes/signup.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] = "POST"){
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+    $email = $_POST['email'];
+
+    try {
+        require_once "dbh.inc.php";
+        require_once "signup_model.inc.php";
+        require_once "signup_contr.inc.php";
+
+        // ERROR HANDLERS
+        if(is_input_empty ($username, $pwd, $email)){
+
+        }
+
+        if(is_email_invalid ($email)){
+
+        }
+
+        if(is_username_taken ( $pdo, $username)){
+
+        }
+
+    } catch (PDOexception $e) {
+        die("connection failed : " . $e->getMessage());
+    }
+
+}else{
+    header("Location: ../index.php");
+
+    die();
+}
---
+++ includes/signup_mode.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+function get_username( object $pdo, string $username)
+{
+    $query = "SELECT username FROM users WHERE username = :username;"; 
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":username", $username);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
