77481ee2f02e49e3c1c4569163152bc857172dd0
Sablu Hasan <150502592+sablu1234@users.noreply.github.com>
2025-04-17 08:16:26 +0600

    UP

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/reset.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h3>Login</h3>
+
+<form action="includes/login.inc.php" method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="pwd" placeholder="Password">
+    <button>Login</button>
+</form>
+
+<h3>Signup</h3>
+
+<form action="includes/signup.inc.php">
+    <input type="text" name="username" placeholder="Username">
+    <input type="password" name="pwd" placeholder="Password">
+    <input type="text" name="email" placeholder="E-Mail">
+    <button>Login</button>
+</form>
+    
+</body>
+</html>
