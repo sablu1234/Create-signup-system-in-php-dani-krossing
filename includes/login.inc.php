16a9477085f9e80d2161e3dcf9ef63ffc11a6e7d
Sablu Hasan <150502592+sablu1234@users.noreply.github.com>
2025-04-22 08:06:32 +0600

    login system

---
+++ includes/login.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] = "POST"){
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    
+
+    try {
+        require_once 'dbh.inc.php';
+        require_once 'login_model.inc.php';
+        require_once 'login_contr.inc.php';
+
+         // ERROR HANDLERS
+     $errors = [];
+
+
+     if(is_input_empty ($username, $pwd))
+     {
+         $errors["empty_input"] = "Fill in all fields!";
+     }
+
+     $result = get_user($pdo, $username);
+
+     if(is_username_wrong($result)){
+        $errors["login_incorrect"] = "incorrect login info!";
+     }
+
+     if(!is_username_wrong($result) && is_password_wrong($pwd, $result["pwd"])){
+        $errors["login_incorrect"] = "incorrect login info!";
+     }
+
+     require_once "config_session.inc.php";
+
+     if($errors){
+         $_SESSION["errors_signup"] = $errors;
+
+         $signupData = [
+             'username' => $username,
+             'email' => $email,
+         ];
+         $_SESSION["signup_data"] = $signupData;
+
+         header("Location: ../index.php");
+         die();
+     }
+    }  catch (PDOexception $e) {
+        die("connection failed : " . $e->getMessage());
+    }
+}else{
+    header("Location: ../index.php");
+
+    die();
+}
